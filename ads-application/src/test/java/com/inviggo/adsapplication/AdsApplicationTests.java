d7f34923f0f8f7c8f7b79e70946f1d8138216b84
Senka <senka.soic@uns.ac.rs>
2021-07-20 12:26:26 +0200

    Backend application initialized

---
+++ ads-application/src/test/java/com/inviggo/adsapplication/AdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inviggo.adsapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
